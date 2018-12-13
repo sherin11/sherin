0bb7cc9213fa63fd1598541d35ba7bc2d1b160f3
sherin11 <41042382+sherin11@users.noreply.github.com>
2018-12-13 14:05:34 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+function validete from[]{
+x=document.get element By Id("emial").value
+if(x==""){
+alert("email must be filled");
+return false;
+}
+{
+x=document.get element By Id("password").value
+if(x=="'){
+alert("password must be filled");
+return false
+}
