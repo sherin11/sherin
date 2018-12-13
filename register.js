52668f8068689ccc11c6d77f4986290ac961e3aa
sherin11 <41042382+sherin11@users.noreply.github.com>
2018-12-13 14:04:10 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,42 @@
+function validate from(){
+x=document get elemet By Id ("email").value
+if(X==""){
+alert("email must be filled");
+return false;
+}
+{
+x=document get elemet By Id ("pass word").value
+if(X==""){
+alert("pass word must be filled");
+return false;
+}
+{
+x=document get elemet By Id ("repect pass word").value
+if(X==""){
+alert("repect pass word must be filled");
+return false;
+}
+{
+x=document get elemet By Id ("branch").value
+if(X==""){
+alert("branch must be filled");
+return false;
+}
+{
+x=document get elemet By Id ("degree").value
+if(X==""){
+alert("degree must be filled");
+return false;
+}
+{
+x=document get elemet By Id ("college name").value
+if(X==""){
+alert("college name must be filled");
+return false;
+}
+{
+x=document get elemet By Id ("user name").value
+if(X==""){
+alert("user name must be filled");
+return false;
+}
